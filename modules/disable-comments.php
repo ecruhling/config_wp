bf35a6f814bb0af163d526c6bb9ff58de84864bb
erik <erik@resourceatlanta.com>
2020-03-02 12:35:08 -0500

    update disable comments module

--- modules/disable-comments.php
+++ modules/disable-comments.php
@@ -12,6 +12,33 @@ function disable_comments() {
 
 add_action( 'init', __NAMESPACE__ . '\\disable_comments' );
 
+/**
+ * Remove various comments feeds
+ */
+
+add_filter( 'feed_links_show_comments_feed', '__return_false' );
+
+function remove_single_comments_feed() {
+	return;
+}
+
+add_filter( 'post_comments_feed_link', __NAMESPACE__ . '\\remove_single_comments_feed' );
+
+function comments_feed_404( $object ) {
+	if ( $object->is_comment_feed ) {
+		wp_die( 'Not Found', '', array(
+			'response'  => 404,
+			'back_link' => true,
+		) );
+	}
+}
+
+add_action( 'parse_query', __NAMESPACE__ . '\\comments_feed_404' );
+
+/**
+ * Hide Comments menu
+ */
+
 function remove_comment_menu() {
 	remove_menu_page( 'edit-comments.php' );          //Comments
 }
---
+++ modules/remove-posts.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace Resource\Config_WP\RemovePosts;
